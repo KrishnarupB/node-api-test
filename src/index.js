d12023aef49aac103039be1f4f9fe96336a1ba83
KrishnarupB <85047556+KrishnarupB@users.noreply.github.com>
2021-07-11 08:39:37 +0530

    1

--- src/index.js
+++ src/index.js
@@ -44,6 +44,6 @@ const server = new ApolloServer({
 
 server
   .listen()
-  .then(({ url }) =>
-    console.log(`Server is running on ${url}`)
+ // .then(({ url }) =>
+ //   console.log(`Server is running on ${url}`)
   );
